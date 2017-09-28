b88d0beb1177c7a0326117b3844a2c0e29a9755e
Ravindra Singh <rratnawa@redhat.com>
2017-09-28 17:14:20 +0530

    env variable APP_SETTINGS TO development

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('APP_SETTINGS')
+config_name = os.getenv('development')
 app = create_app(config_name)
 
 if __name__ == '__main__':
